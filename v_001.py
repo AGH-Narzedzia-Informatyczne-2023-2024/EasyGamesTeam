d09bc16e575692a9cc4a5f3eefacfd007bd86a7d
pavlvs-91 <gleindekp@gmail.com>
2023-11-15 00:34:17 +0100

    I've created a branch for very first version of our game and dropped there first file

---
+++ v_001.py
@@ -0,0 +1,13 @@
+print("Witaj w naszej grze!\n")
+print("1. Papier\n")
+print("2. Kamien\n")
+print("3. Nozyce\n")
+
+wybor=input("Wybierz swoja opcje:")
+
+if wybor==1:
+    print("Ooops, ja wybralem nozyce, przegrywasz :\))")
+if wybor==2:
+    print("Ooops, ja wybralem papier, przegrywasz :\))")
+if wybor==3:
+    print("Ooops, ja wybralem kamien, przegrywasz :\))")
