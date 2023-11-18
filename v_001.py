95ce1d5f8a357ce8cee13bad5120e4814573487e
Valadar <vworkgorelikov@gmail.com>
2023-11-18 14:46:55 +0100

    test commit

--- v_001.py
+++ v_001.py
@@ -1,13 +1,1 @@
-print("Witaj w naszej grze!\n")
-i=1
-while i==1:
-    print("1. Papier\n")
-    print("2. Kamien\n")
-    print("3. Nozyce\n")
-    wybor=int(input("Wybierz swoja opcje:"))
-    if wybor==1:
-        print("Ooops, ja wybralem nozyce, przegrywasz :))\n")
-    if wybor==2:
-        print("Ooops, ja wybralem papier, przegrywasz :))\n")
-    if wybor==3:
-        print("Ooops, ja wybralem kamien, przegrywasz :))\n")
+#test commit
