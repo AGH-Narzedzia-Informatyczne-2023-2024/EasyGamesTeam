371e9c6e160664847e0b84258c35110f7b3600bb
Valadar <vworkgorelikov@gmail.com>
2023-11-18 15:13:02 +0100

    update gamemodes

--- v_001.py
+++ v_001.py
@@ -1,7 +1,6 @@
 import random
 
 print("Witaj w naszej grze!\n")
-game_mode = 0
 print("choose game mode:"
       "\nto easy(0)"
       "\nrandom(1)"
@@ -21,6 +20,20 @@ if (game_mode == 0):
             print("Ooops, ja wybralem kamien, przegrywasz :))\n")
 
 if (game_mode == 1):
-    None
+    while True:
+        print("1. Papier\n")
+        print("2. Kamien\n")
+        print("3. Nozyce\n")
+        wybor = int(input("Wybierz swoja opcje:"))
+        comp_choose = random.randint(1, 3)
+        if (wybor == comp_choose):
+            print(f"remis-wybrales:{wybor},wybralem{comp_choose}")
+        if ((wybor == 1 and comp_choose == 2) or (wybor == 2 and comp_choose == 3) or (
+                wybor == 3 and comp_choose == 1)):
+            print(f"wygrales-wybrales:{wybor},wybralem{comp_choose}")
+        if ((wybor == 2 and comp_choose == 1) or (wybor == 3 and comp_choose == 2) or (
+                wybor == 1 and comp_choose == 3)):
+            print(f"przegrales-wybrales:{wybor},wybralem{comp_choose}")
+
 if (game_mode == 2):
     None
